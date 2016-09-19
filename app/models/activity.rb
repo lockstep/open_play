c19bbcf99d43709cc66400fe097043aee7aa66c4
GotZilaZa <kirati@tripler.co.th>
2016-09-19 17:32:28 +0700

    add activity creation page (#8)

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,12 @@
 class Activity < ApplicationRecord
   belongs_to :business
+  validates_presence_of :name
+  validate :end_time_is_after_start_time
+
+  def end_time_is_after_start_time
+    if ( end_time.seconds_since_midnight.to_i <=
+      start_time.seconds_since_midnight.to_i)
+      errors.add(:end_time, 'must be after the start time')
+    end
+  end
 end
