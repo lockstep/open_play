9b716d3574a512107af415c1d11e6964368b1ec2
GotZilaZa <kirati@tripler.co.th>
2016-09-19 11:49:34 +0700

    changed data type for business description (#9)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160914134308) do
+ActiveRecord::Schema.define(version: 20160919043215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20160914134308) do
 
   create_table "businesses", force: :cascade do |t|
     t.string   "name"
-    t.string   "description"
+    t.text     "description"
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
