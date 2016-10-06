15af787569f23e20a8d521f447041d9073d66a87
gotzilaza <kirati.samat@gmail.com>
2016-10-06 15:41:11 +0700

    added support for ES6

---
+++ config/initializers/sprockets_commoner.rb
@@ -0,0 +1,13 @@
+Rails.application.config.assets.configure do |env|
+  Sprockets::Commoner::Processor.configure(env,
+    # include, exclude, and babel_exclude patterns can be path prefixes or regexes.
+    # Explicitely list paths to include. The default is `[env.root]`
+    include: ['app/assets/javascripts/'],
+    # List files to ignore and not process require calls or apply any Babel
+    # transforms to. Default is ['vendor/bundle'].
+    exclude: ['vendor/assets', /ignored/],
+    # Anything listed in babel_exclude has its require calls resolved, but no
+    # transforms listed in .babelrcs applied. Default is [/node_modules/]
+    babel_exclude: [/node_modules/]
+  )
+end
