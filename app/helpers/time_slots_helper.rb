1a52503ea23398e3084429ad2170be70ecc740fa
Sasitorn Aramcharoenrat <sasitorn.oeam@gmail.com>
2016-10-14 13:15:19 +0700

    Refactoring time slot helper's code

--- app/helpers/time_slots_helper.rb
+++ app/helpers/time_slots_helper.rb
@@ -1,16 +1,16 @@
 module TimeSlotsHelper
-  def build_time_slots(reservable, booking_date, booking_time)
-    booking_date = DateTime.parse(booking_date)
-    requested_time = booking_date + Time.parse(booking_time)
+  def build_time_slots(reservable, requested_date, requested_time)
+    requested_date = DateTime.parse(requested_date)
+    requested_time = requested_date + Time.parse(requested_time)
       .seconds_since_midnight.seconds
-    closing_time = merge_date_and_time(booking_date, reservable.end_time)
+    closing_time = merge_date_and_time(requested_date, reservable.end_time)
     time_slots = []
     subsequent_time = requested_time
     while subsequent_time < closing_time do
       time_slots.push(
         {
           time: subsequent_time,
-          available: available_booking_time?(reservable, subsequent_time)
+          available: reservable_available_to_book?(reservable, subsequent_time)
         }
       )
       subsequent_time += reservable.interval.minutes
@@ -20,19 +20,29 @@ module TimeSlotsHelper
 
   private
 
-  def available_booking_time?(reservable, time)
-    booking_date = time.beginning_of_day
-    opening_time = merge_date_and_time(booking_date, reservable.start_time)
-    closing_time = merge_date_and_time(booking_date, reservable.end_time)
-    return false unless time >= opening_time && time < closing_time
-    bookings = reservable.bookings.where(booking_date: time)
+  def reservable_available_to_book?(reservable, requested_time)
+    return false unless reservable_opening?(reservable, requested_time)
+    reservable_available?(reservable, requested_time)
+  end
+
+  def reservable_opening?(reservable, requested_time)
+    requested_date = requested_time.beginning_of_day
+    opening_time = merge_date_and_time(requested_date, reservable.start_time)
+    closing_time = merge_date_and_time(requested_date, reservable.end_time)
+    inWorkingHours = (requested_time >= opening_time) && (requested_time < closing_time)
+  end
+
+  def reservable_available?(reservable, requested_time)
+    requested_date = requested_time.beginning_of_day
+    bookings = reservable.bookings.where(booking_date: requested_time)
     return true unless bookings.present?
     bookings.each do |booking|
-      start_booking_time = merge_date_and_time(booking_date, booking.start_time)
-      end_booking_time = merge_date_and_time(booking_date, booking.end_time)
-      return false unless (time < start_booking_time) || (time >= end_booking_time)
+      start_booking_time = merge_date_and_time(requested_date, booking.start_time)
+      end_booking_time = merge_date_and_time(requested_date, booking.end_time)
+      availableToBook = (requested_time < start_booking_time) || (requested_time >= end_booking_time)
+      return false unless availableToBook
     end
-    return true
+    true
   end
 
   def merge_date_and_time(date, time)
