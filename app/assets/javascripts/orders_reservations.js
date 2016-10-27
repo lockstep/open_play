b8b31208c48a2a2ff8635a66089327c7f1361b51
gunosk129 <gunosk129@users.noreply.github.com>
2016-10-28 06:38:52 +0700

    change date format to show on reservation page (#46)

--- app/assets/javascripts/orders_reservations.js
+++ app/assets/javascripts/orders_reservations.js
@@ -1,9 +1,9 @@
 $(function() {
   if ($('#reservations-booking-date').length > 0) {
     var datePicker = new Pikaday({
-      field: document.getElementById('reservations-booking-date')
+      field: document.getElementById('reservations-booking-date'),
+      format: 'D MMM YYYY'
     });
-
     $('#reservations-booking-date').on('change', function() {
       $('#orders-reservations-form').submit()
     });
