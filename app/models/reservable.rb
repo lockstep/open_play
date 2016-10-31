eaeed94df66d50b2c002d90ad06cdaa2a3e38363
Sasitorn Aramcharoenrat <sasitorn.oeam@gmail.com>
2016-10-31 15:34:07 +0700

    Fixed code to use guard statement

--- app/models/reservable.rb
+++ app/models/reservable.rb
@@ -24,7 +24,9 @@ class Reservable < ApplicationRecord
 
   def end_time_is_after_start_time
     return unless start_time.present? && end_time.present?
-    errors.add(:end_time, 'must be after the start time') if start_time > end_time
+    return unless start_time > end_time
+
+    errors.add(:end_time, 'must be after the start time')
   end
 
   def number_of_booked_players(start_time, end_time, date)
