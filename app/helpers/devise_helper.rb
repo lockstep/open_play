f0ae57f95869d8e84b6967a5a55f70af90aaa39e
gotzilaza <kirati.samat@gmail.com>
2016-11-22 16:26:39 +0700

    updated nice error explanation

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,25 @@
+module DeviseHelper
+  def devise_error_messages!
+    return "" unless devise_error_messages?
+
+    messages = resource.errors.full_messages.map do |msg|
+      content_tag(:div, msg, class: 'hello')
+    end.join
+
+    html = <<-HTML
+    <div class='row'>
+      <div class='col-xs-4 offset-xs-4'>
+        <div id="error_explanation" class="alert alert-danger" role="alert">
+          #{messages}
+        </div>
+      </div>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
+  def devise_error_messages?
+    !resource.errors.empty?
+  end
+end
