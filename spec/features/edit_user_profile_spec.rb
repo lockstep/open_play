8eb8e063dfe16f9f64f6b095e14cd56b31895ab3
gotzilaza <kirati.samat@gmail.com>
2016-11-22 15:50:53 +0700

    added phone_number to edit user form

--- spec/features/edit_user_profile_spec.rb
+++ spec/features/edit_user_profile_spec.rb
@@ -10,28 +10,11 @@ feature 'Edit User Profile' do
       complete_user_form
 
       expect(page).to have_content 'Successfully updated user profile'
+      expect(current_path).to eq user_path(@user)
     end
   end
 
   context 'some params are invalid' do
-    context 'email is omitted' do
-      scenario 'unsuccessfully edited user profile' do
-        navigate_to_edit_user_form
-        complete_user_form(email: '')
-
-        expect(page).to have_content "can't be blank"
-      end
-    end
-
-    context 'password is omitted' do
-      scenario 'unsuccessfully edited user profile' do
-        navigate_to_edit_user_form
-        complete_user_form(password: '')
-
-        expect(page).to have_content "can't be blank"
-      end
-    end
-
     context 'password_confirmation does not match password' do
       scenario 'unsuccessfully edited user profile' do
         navigate_to_edit_user_form
@@ -48,11 +31,14 @@ feature 'Edit User Profile' do
       click_button @user.email
     end
     click_link 'Your Profile'
+    click_link 'Edit Profile'
   end
 
   def complete_user_form(overrides={})
     within '#edit_user_form' do
-      fill_in 'user[email]', with: overrides[:email] || 'peter@gmail.com'
+      fill_in 'user[first_name]', with: overrides[:first_name] || 'eric'
+      fill_in 'user[last_name]', with: overrides[:first_name] || 'schmidt'
+      fill_in 'user[phone_number]', with: overrides[:phone_number] || '+1 650-253-0000'
       fill_in 'user[password]', with: overrides[:password] || '123456'
       fill_in 'user[password_confirmation]', with: overrides[:password_confirmation] || '123456'
       click_button 'Update'
