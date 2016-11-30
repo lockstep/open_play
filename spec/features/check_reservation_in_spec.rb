f1343250c1a1d0294b6e5d2809a0676d34358d1c
Sasitorn Aramcharoenrat <sasitorn.oeam@gmail.com>
2016-11-30 15:37:03 +0700

    OP-105 Fixed bug when trying to cancel or check in the reservation

--- spec/features/check_reservation_in_spec.rb
+++ spec/features/check_reservation_in_spec.rb
@@ -10,8 +10,13 @@ feature 'Check Reservation In', :js do
   context 'bookings exist' do
     background do
       order = create(:order, user: @user, activity: @bowling)
-      @booking = create(:booking, order: order, reservable: @lane,
-        booking_date: '2016-10-13')
+      @booking = create(
+        :booking,
+        order: order,
+        reservable: @lane,
+        number_of_players: 5,
+        booking_date: '2016-10-13'
+      )
     end
 
     context 'booking_date and system date are not the same' do
@@ -40,6 +45,20 @@ feature 'Check Reservation In', :js do
           expect(page).to_not have_link 'Check in'
           expect(page).to_not have_link 'Cancel'
           expect(page).to have_css("#booking_#{@booking.id}.table-success")
+        end
+        context "Number of players in the reservation is more than current reservable's available players" do
+          before do
+            reservable = @booking.reservable
+            reservable.update(maximum_players: 5)
+          end
+          scenario 'skips the validation and successfully checked in' do
+            visit root_path
+            click_link 'Manage Business'
+            click_link 'View reservations'
+            select_a_booking_date('2016-10-13')
+            click_link 'Check in'
+            expect(@booking.reload.checked_in).to eq true
+          end
         end
       end
     end
