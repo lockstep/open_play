fcfd90b3ba146b2c1ef8ac10b345bc0210884e57
Michael Kohl <citizen428@gmail.com>
2017-08-03 13:36:01 +0700

    Fix businesses seed

--- db/seeds/02_businesses.rb
+++ db/seeds/02_businesses.rb
@@ -1,7 +1,7 @@
 user1 = User.find_by(email: 'user1@example.com')
 Business.where(name: 'Disney Land').first_or_create do |business|
   business.phone_number = 1234567890
-  business.address = '123 building'
+  business.address_line_one = '123 building'
   business.description = 'amazing amusement park'
   business.user = user1
 end
@@ -9,7 +9,7 @@ end
 user2 = User.find_by(email: 'user2@example.com')
 Business.where(name: 'Country Club').first_or_create do |business|
   business.phone_number = 1234567890
-  business.address = '123 building'
+  business.address_line_one = '123 building'
   business.description = 'amazing amusement park'
   business.user = user2
 end
